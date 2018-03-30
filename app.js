fac89d6d9bca6a5b2188501a757898e84a1f4784
matt babcock <babcockmatthew12@yahoo.com>
2018-03-29 19:52:06 -0700

    another one

--- app.js
+++ app.js
@@ -8,9 +8,22 @@ function renderButtons() {
 for (i = 0; i < topics.length; i++) { 
     var btn = $("<button>");
     btn.addClass("topic");
-    btn.attr("data-name", topics[i]);
+    btn.attr("id", topics[i]);
     btn.text(topics[i]);
     $("#buttons-view").append(btn);
 }
 }
 renderButtons();
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Z0zBY7wum0yjVC4oXH88nferaJ8cVSfl&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
+
+apiKey = "Z0zBY7wum0yjVC4oXH88nferaJ8cVSfl"
+
+gipyURL = "http://api.giphy.com/v1/gifs/search?q="+ "id" +"&api_key" + apiKey +"&limit=10"
+
+
+$.ajax({
+    url: giphyURL,
+    method: "GET"
+  })
